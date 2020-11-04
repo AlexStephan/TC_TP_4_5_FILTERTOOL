2e15d77593db40d32d3426808e9183cb985e1ee8
MTripo <mtripoli@itba.edu.ar>
2020-11-04 18:00:21 -0300

    Butterworth v1.0

--- src/backend/Approx/Legendre.py
+++ src/backend/Approx/Legendre.py
@@ -2,7 +2,7 @@ from src.backend.Filter.Filter import *
 from scipy import signal
 import numpy as np
 
-class Butterworth(object):
+class Legendre(object):
     def __init__(self, filter: Filter):
         self.filter = filter
         self.type = self.filter.get_type(self.filter)
@@ -15,64 +15,65 @@ class Butterworth(object):
         self.den = None
         self.sos = None
 
+    def get_Data(self):
+        data = {FilterData.Aa: None, FilterData.faMin: None,
+                FilterData.Ap: None, FilterData.fpMin: None,
+                FilterData.gain: None, FilterData.Nmax: None,
+                FilterData.Nmin: None, FilterData.Qmax: None,
+                FilterData.Denorm: None}
+        data = self.filter.get_reqData(self.filter)
+        return data
+
     def calc_Order(self):
         val, msg = self.filter.validate(self.filter)
         if val is False:
             return msg
-
-        fpMin = self.filter.reqData[FilterData.fpMin.value]
-        fpMax = self.filter.reqData[FilterData.fpMax.value]
-        Ap = self.filter.reqData[FilterData.Ap.value]
-
-        faMin = self.filter.reqData[FilterData.faMin.value]
-        faMax = self.filter.reqData[FilterData.fpMax.value]
-        Aa = self.filter.reqData[FilterData.Aa.value]
-
-        Nmin = self.filter.reqData[FilterData.Nmin.value]
-        Nmax = self.filter.reqData[FilterData.Nmax.value]
-
         if self.type is "Low Pass":
-            order, wo = signal.buttord(2 * np.pi * fpMin, 2 * np.pi * faMin, Ap, Aa, analog=True)
-            if Nmin > order:
-                order = Nmin
-                self.order = Nmin
-            elif Nmax < order:
-                order = Nmax
-                self.order = Nmax
+            data = self.get_Data(self)
+            order, wo = signal.buttord(2 * np.pi * data.fpMin, 2 * np.pi * data.faMin, data.Ap, data.Aa, analog=True)
+            if data.Nmin > order:
+                order = data.Nmin
+                self.order = data.Nmin
+            elif data.Nmax < order:
+                order = data.Nmax
+                self.order = data.Nmax
             else:
                 self.order = order
         elif self.type is "High Pass":
-            order, wo = signal.buttord(2 * np.pi * fpMin, 2 * np.pi * faMin, Ap, Aa, analog=True)
-            if Nmin > order:
-                order = Nmin
-                self.order = Nmin
-            elif Nmax < order:
-                order = Nmax
-                self.order = Nmax
+            data = self.get_Data(self)
+            order, wo = signal.buttord(2 * np.pi * data.fpMin, 2 * np.pi * data.faMin, data.Ap, data.Aa, analog=True)
+            if data.Nmin > order:
+                order = data.Nmin
+                self.order = data.Nmin
+            elif data.Nmax < order:
+                order = data.Nmax
+                self.order = data.Nmax
             else:
                 self.order = order
         elif self.type is "Band Pass":
-            order, wo = signal.buttord([2 * np.pi * fpMin, 2 * np.pi * fpMax],
-                                            [2 * np.pi * faMin, 2 * np.pi * faMax],
-                                            Ap, Aa, analog=True)
-            if Nmin > order:
-                order = Nmin
-                self.order = Nmin
-            elif Nmax < order:
-                order = Nmax
-                self.order = Nmax
+            data = self.get_Data(self)
+            order, wo = signal.buttord([2 * np.pi * data.fpMin, 2 * np.pi * data.fpMax],
+                                            [2 * np.pi * data.faMin, 2 * np.pi * data.faMax],
+                                            data.Ap, data.Aa, analog=True)
+            if data.Nmin > order:
+                order = data.Nmin
+                self.order = data.Nmin
+            elif data.Nmax < order:
+                order = data.Nmax
+                self.order = data.Nmax
             else:
                 self.order = order
         elif self.type is "Band Reject":
-            order, wo = signal.buttord([2 * np.pi * fpMin, 2 * np.pi * fpMax],
-                                            [2 * np.pi * faMin, 2 * np.pi * faMax],
-                                            Ap, Aa, analog=True)
-            if Nmin > order:
-                order = Nmin
-                self.order = Nmin
-            elif Nmax < order:
-                order = Nmax
-                self.order = Nmax
+            data = self.get_Data(self)
+            order, wo = signal.buttord([2 * np.pi * data.fpMin, 2 * np.pi * data.fpMax],
+                                            [2 * np.pi * data.faMin, 2 * np.pi * data.faMax],
+                                            data.Ap, data.Aa, analog=True)
+            if data.Nmin > order:
+                order = data.Nmin
+                self.order = data.Nmin
+            elif data.Nmax < order:
+                order = data.Nmax
+                self.order = data.Nmax
             else:
                 self.order = order
         else:
@@ -83,40 +84,33 @@ class Butterworth(object):
         val, msg = self.filter.validate(self.filter)
         if val is False:
             return msg
-
-        fpMin = self.filter.reqData[FilterData.fpMin.value]
-        fpMax = self.filter.reqData[FilterData.fpMax.value]
-        Ap = self.filter.reqData[FilterData.Ap.value]
-
-        faMin = self.filter.reqData[FilterData.faMin.value]
-        faMax = self.filter.reqData[FilterData.fpMax.value]
-        Aa = self.filter.reqData[FilterData.Aa.value]
-
-        Denorm = self.filter.reqData[FilterData.Denorm.value]
-
         if self.type is "Low Pass":
-            fo1 = fpMin / ((10 ** (Ap / 10) - 1) ** (1 / (2 * self.order)))
-            fo2 = faMin / ((10 ** (Aa / 10) - 1) ** (1 / (2 * self.order)))
-            self.fo = 10 ** (np.log10(fo1) * (1 - Denorm / 100) + np.log10(fo2) * Denorm / 100)
+            data = self.get_Data(self)
+            fo1 = data.fpMin / ((10 ** (data.Ap / 10) - 1) ** (1 / (2 * self.order)))
+            fo2 = data.faMin / ((10 ** (data.Aa / 10) - 1) ** (1 / (2 * self.order)))
+            self.fo = 10 ** (np.log10(fo1) * (1 - data.Denorm / 100) + np.log10(fo2) * data.Denorm / 100)
         elif self.type is "High Pass":
-            fo1 = fpMin * ((10 ** (Ap / 10) - 1) ** (1 / (2 * self.order)))
-            fo2 = faMin * ((10 ** (Aa / 10) - 1) ** (1 / (2 * self.order)))
-            self.fo = 10 ** (np.log10(fo1) * (1 - Denorm / 100) + np.log10(fo2) * Denorm / 100)
+            data = self.get_Data(self)
+            fo1 = data.fpMin * ((10 ** (data.Ap / 10) - 1) ** (1 / (2 * self.order)))
+            fo2 = data.faMin * ((10 ** (data.Aa / 10) - 1) ** (1 / (2 * self.order)))
+            self.fo = 10 ** (np.log10(fo1) * (1 - data.Denorm / 100) + np.log10(fo2) * data.Denorm / 100)
         elif self.type is "Band Pass":
-            fop1 = fpMin * (10 ** (Ap / 10) - 1) ** (1 / self.order)
-            foa1 = faMin * (10 ** (Aa / 10) - 1) ** (1 / self.order)
-            fop2 = fpMax / (10 ** (Ap / 10) - 1) ** (1 / self.order)
-            foa2 = faMax / (10 ** (Aa / 10) - 1) ** (1 / self.order)
-            fo1 = 10 ** (np.log10(fop1) * (1 - Denorm / 100) + np.log10(foa1) * Denorm / 100)
-            fo2 = 10 ** (np.log10(fop2) * (1 - Denorm / 100) + np.log10(foa2) * Denorm / 100)
+            data = self.get_Data(self)
+            fop1 = data.fpMin * (10 ** (data.Ap / 10) - 1) ** (1 / self.order)
+            foa1 = data.faMin * (10 ** (data.Aa / 10) - 1) ** (1 / self.order)
+            fop2 = data.fpMax / (10 ** (data.Ap / 10) - 1) ** (1 / self.order)
+            foa2 = data.faMax / (10 ** (data.Aa / 10) - 1) ** (1 / self.order)
+            fo1 = 10 ** (np.log10(fop1) * (1 - data.Denorm / 100) + np.log10(foa1) * data.Denorm / 100)
+            fo2 = 10 ** (np.log10(fop2) * (1 - data.Denorm / 100) + np.log10(foa2) * data.Denorm / 100)
             self.fo = np.sqrt(fo1 * fo2)
         elif self.type is "Band Reject":
-            fop1 = fpMin / (10 ** (Ap / 10) - 1) ** (1 / self.order)
-            foa1 = faMin / (10 ** (Aa / 10) - 1) ** (1 / self.order)
-            fop2 = fpMax * (10 ** (Ap / 10) - 1) ** (1 / self.order)
-            foa2 = faMax * (10 ** (Aa / 10) - 1) ** (1 / self.order)
-            fo1 = 10 ** (np.log10(fop1) * (1 - Denorm / 100) + np.log10(foa1) * Denorm / 100)
-            fo2 = 10 ** (np.log10(fop2) * (1 - Denorm / 100) + np.log10(foa2) * Denorm / 100)
+            data = self.get_Data(self)
+            fop1 = data.fpMin / (10 ** (data.Ap / 10) - 1) ** (1 / self.order)
+            foa1 = data.faMin / (10 ** (data.Aa / 10) - 1) ** (1 / self.order)
+            fop2 = data.fpMax * (10 ** (data.Ap / 10) - 1) ** (1 / self.order)
+            foa2 = data.faMax * (10 ** (data.Aa / 10) - 1) ** (1 / self.order)
+            fo1 = 10 ** (np.log10(fop1) * (1 - data.Denorm / 100) + np.log10(foa1) * data.Denorm / 100)
+            fo2 = 10 ** (np.log10(fop2) * (1 - data.Denorm / 100) + np.log10(foa2) * data.Denorm / 100)
             self.fo = np.sqrt(fo1 * fo2)
         else:
             message = "Error: Enter Filter Type."
@@ -166,11 +160,6 @@ class Butterworth(object):
     def get_zpk(self):
         return self.z, self.p, self.k
 
-    def get_zpGk(self):
-        gain = self.filter.reqData[FilterData.gain.value]
-        Gk = self.k * 10 ** (gain / 20)
-        return self.z, self.p, Gk
-
     def get_sos(self):
         val, msg = self.filter.validate(self.filter)
         if val is False:
@@ -191,81 +180,52 @@ class Butterworth(object):
             message = "Error: Enter Filter Type."
             return message
 
-    def get_TransFuncWithGain(self):                                        #return angular frequency and H(s)
+    def get_TransFunc(self):                                        #return angular frequency and H(s)
         val, msg = self.filter.validate(self.filter)
         if val is False:
             return msg
-
-        gain = self.filter.reqData[FilterData.gain.value]
-        z, p, k = self.get_zpk(self)
-
         if self.type is "Low Pass":
+            data = self.get_Data(self)
+            z, p, k = self.get_zpk(self)
             sys = signal.ZerosPolesGain(z, p, k)
             w, mag, pha = signal.bode(sys)
-            for i in range(len(mag)):
-                mag[i] = mag[i] + gain
+            mag = mag * 10 ** (data.gain / 20)
             return w, mag, pha
         elif self.type is "High Pass":
+            data = self.get_Data(self)
+            z, p, k = self.get_zpk(self)
             sys = signal.ZerosPolesGain(z, p, k)
             w, mag, pha = signal.bode(sys)
-            for i in range(len(mag)):
-                mag[i] = mag[i] + gain
+            mag = mag * 10 ** (data.gain / 20)
             return w, mag, pha
         elif self.type is "Band Pass":
+            data = self.get_Data(self)
+            z, p, k = self.get_zpk(self)
             sys = signal.ZerosPolesGain(z, p, k)
             w, mag, pha = signal.bode(sys)
-            for i in range(len(mag)):
-                mag[i] = mag[i] + gain
+            mag = mag * 10 ** (data.gain / 20)
             return w, mag, pha
         elif self.type is "Band Reject":
+            data = self.get_Data(self)
+            z, p, k = self.get_zpk(self)
             sys = signal.ZerosPolesGain(z, p, k)
             w, mag, pha = signal.bode(sys)
-            for i in range(len(mag)):
-                mag[i] = mag[i] + gain
-            return w, mag, pha
-        else:
-            message = "Error: Enter Filter Type."
-            return message
-
-    def get_TransFuncWithoutGain(self):  # return angular frequency and H(s)
-        val, msg = self.filter.validate(self.filter)
-        if val is False:
-            return msg
-
-        z, p, k = self.get_zpk(self)
-
-        if self.type is "Low Pass":
-            sys = signal.ZerosPolesGain(z, p, k)
-            w, mag, pha = signal.bode(sys)
-            return w, mag, pha
-        elif self.type is "High Pass":
-            sys = signal.ZerosPolesGain(z, p, k)
-            w, mag, pha = signal.bode(sys)
-            return w, mag, pha
-        elif self.type is "Band Pass":
-            sys = signal.ZerosPolesGain(z, p, k)
-            w, mag, pha = signal.bode(sys)
-            return w, mag, pha
-        elif self.type is "Band Reject":
-            sys = signal.ZerosPolesGain(z, p, k)
-            w, mag, pha = signal.bode(sys)
+            mag = mag * 10 ** (data.gain / 20)
             return w, mag, pha
         else:
             message = "Error: Enter Filter Type."
             return message
 
     def check_Q(self) -> bool:
-
-        Qmax = self.filter.reqData[FilterData.Qmax.value]
-
-        if self.order > 1 and Qmax is not None:
+        if self.order > 1:
+            data = self.get_Data(self)
             z, p, k = self.get_zpk(self)
             q_arr = []
             for pole in p:
                 q = abs(abs(pole) / (2 * pole.real))
                 q_arr.append(q)
             q_sys = np.max(q_arr)
-            if q_sys > Qmax and self.order > 1:
+            if q_sys > data.Qmax and self.order > 1:
                 self.order = self.order - 1
                 return False
             else:
@@ -280,4 +240,4 @@ class Butterworth(object):
         while self.check_Q(self) is False:
             self.calc_fo(self)
             self.calc_zpk(self)
-        return self.get_TransFuncWithGain(self)
+        return self.get_TransFunc(self)
