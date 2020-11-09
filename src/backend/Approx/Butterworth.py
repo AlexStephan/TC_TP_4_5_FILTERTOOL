7af79340b38527327d3922b9b03dd29ef2815ac6
MTripo <mtripoli@itba.edu.ar>
2020-11-09 19:49:25 -0300

    correction v4.2.1

--- src/backend/Approx/Butterworth.py
+++ src/backend/Approx/Butterworth.py
@@ -419,12 +419,12 @@ class Butterworth(object):
         self.wgd = w
 
     def calc_Impulse_Response(self):
-        t, out = signal.impulse2(self.get_lti(), T=np.linspace(0, 1, num=10000))
+        t, out = signal.impulse2(self.get_lti(), T=np.linspace(0, 10, num=10000))
         self.timp = t
         self.impresp = out
 
     def calc_Step_Response(self):
-        t, out = signal.step2(self.get_lti(), T=np.linspace(0, 1, num=10000))
+        t, out = signal.step2(self.get_lti(), T=np.linspace(0, 10, num=10000))
         self.tstep = t
         self.stepresp = out
 
